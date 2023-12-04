2cf6604f1f5a85c5875d318d62dc773f1e197c76
krypek <115574014+krypciak@users.noreply.github.com>
2023-12-04 13:57:43 +0100

    Fix addon installer thinking nvda is running when it's not on non-english systems

--- src/tts/tts-nvda.ts
+++ src/tts/tts-nvda.ts
@@ -82,13 +82,13 @@ export class AddonInstaller {
     }
 
     private static async isNvdaRunning(): Promise<boolean> {
-        function isProcessRunning(name: string): Promise<boolean> {
-            return new Promise((resolve) => {
-                require('child_process').exec('wmic process where "name=\'' + name + '\'" get ExecutablePath', (_: string, __: string, stderr: string) => {
-                    resolve(stderr.trim() != 'No Instance(s) Available.')
-                })
-            })
-        }
+	function isProcessRunning(name: string): Promise<boolean> {
+	    return new Promise((resolve) => {
+		require('child_process').exec(`qprocess "${name}"`, (_: string, __: string, stderr: string) => {
+		    resolve(!stderr.trim())
+		})
+	    })
+	}
         return isProcessRunning('nvda.exe')
     }
 
