46db41b88ad600e94930a582dec57d18e7a478a4
krypek <krypek@tuta.io>
2023-12-17 21:19:02 +0100

    Revert "Add logging to the bundled ccloader"
    This reverts commit e9b94b21f7fe89db7dbcb25204a4f4e2f363c6b1.

--- bundle.sh
+++ bundle.sh
@@ -17,8 +17,6 @@ cp -r pack/*CCLoader*/* 'pack/'
 rm -rf pack/*CCLoader*
 rm -rf pack/ccloader.tar.gz
 
-patch -p1 pack/ccloader/js/ccloader.js < .github/workflows/addLogging.patch
-
 cd pack
 rm -rf ../bundle.zip
 zip -r ../bundle.zip *
