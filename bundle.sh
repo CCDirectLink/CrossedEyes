8482be3e3cb23f2fe1e95c349b85b7bbc4076fc8
krypek <krypek@tuta.io>
2024-02-06 13:15:16 +0100

    Add cc-diorbital-menu to bundle.zip

--- bundle.sh
+++ bundle.sh
@@ -3,11 +3,15 @@
 blitzkrieg_version="$(jq '.dependencies["cc-blitzkrieg"]' ccmod.json | tail -c +4 | head -c -2)"
 blitzkrieg_url="https://github.com/krypciak/cc-blitzkrieg/releases/download/v${blitzkrieg_version}/cc-blitzkrieg-${blitzkrieg_version}.ccmod"
 
+dior_version="$(jq '.dependencies["cc-diorbital-menu"]' ccmod.json | tail -c +4 | head -c -2)"
+dior_url="https://github.com/krypciak/cc-diorbital-menu/releases/download/v${dior_version}/cc-diorbital-menu-${dior_version}.ccmod"
+
 inputapi_url='https://github.com/CCDirectLink/input-api/releases/download/v1.0.1/input-api.ccmod'
 ccloader_url="$(curl -s 'https://api.github.com/repos/CCDirectLink/CCLoader/releases/latest' | jq '.tarball_url' | head -c -2 | tail -c +2)"
 
 mkdir -p pack/assets/mods
 [ ! -f pack/assets/mods/cc-blitzkrieg* ] && wget "${blitzkrieg_url}" -O "pack/assets/mods/cc-blitzkrieg-${blitzkrieg_version}.ccmod"
+[ ! -f pack/assets/mods/cc-diorbital-menu* ] && wget "${dior_url}" -O "pack/assets/mods/cc-diorbital-menu-${dior_version}.ccmod"
 [ ! -f pack/assets/mods/input-api* ] && wget "${inputapi_url}" -O 'pack/assets/mods/input-api.ccmod'
 cp crossedeyes*.ccmod pack/assets/mods/
 
