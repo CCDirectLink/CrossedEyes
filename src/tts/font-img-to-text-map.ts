430080b26dd504d7474b92fe1dda1cdbc999179a
krypek <krypek@tuta.io>
2024-02-28 19:19:51 +0100

    Improve FontToImgText format

--- src/tts/font-img-to-text-map.ts
+++ src/tts/font-img-to-text-map.ts
@@ -4,18 +4,14 @@ export class FontToImgMap {
     private static map: Record<string, string>
 
     public static convert(name: string): string {
-        if (name == 'help') return this.convert('gamepad-pause')
-        if (name == 'help2') return this.convert('gamepad-x')
-        if (name == 'help3') return this.convert('gamepad-y')
-        if (name == 'help4') return this.convert('right-stick-press')
-
         if (sc.fontsystem.gamepadIconStyle == sc.GAMEPAD_ICON_STYLE.PS3) {
             if (name == 'gamepad-x') return Lang.menu.fontImgToTextMap['gamepad-dualshock-x']
             if (name == 'gamepad-y') return Lang.menu.fontImgToTextMap['gamepad-dualshock-y']
             if (name == 'gamepad-b') return Lang.menu.fontImgToTextMap['gamepad-dualshock-b']
             if (name == 'gamepad-a') return Lang.menu.fontImgToTextMap['gamepad-dualshock-a']
         }
-        const rep = FontToImgMap.map[name]
+        let rep = FontToImgMap.map[name]
+        if (rep?.startsWith('!')) rep = this.convert(rep.substring(1))
         return rep
     }
 
@@ -30,6 +26,25 @@ export class FontToImgMap {
                 return obj
             })(),
             ...Lang.menu.fontImgToTextMap,
+            help: '!gamepad-pause',
+            help2: '!gamepad-x',
+            help3: '!gamepad-y',
+            help4: '!right-stick-press',
+            'circle-left': '!gamepad-l1',
+            dash: '!gamepad-l1',
+            'circle-right': 'gamepad-r1',
+            throw: 'gamepad-r1',
+            quick: 'gamepad-l2',
+            special: '!gamepad-r2',
+            menu: '!gamepad-select',
+            guard: '!gamepad-a',
+            back: '!gamepad-b',
+            'skip-cutscene': '!gamepad-y',
+            'item-helm-scale': '!item-helm',
+            'item-sword-scale': '!item-sword',
+            'item-sword-rare': '!item-sword',
+            'item-belt-scale': '!item-belt',
+            'item-shoe-scale': '!item-shoe',
             'language-0': '',
             'language-1': '',
             'language-2': '',
