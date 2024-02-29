5d8c92de5adfe0001a41cfe67bc9933840616f0c
krypek <krypek@tuta.io>
2024-02-29 19:21:18 +0100

    Fix quick menu layout force issues

--- src/options.ts
+++ src/options.ts
@@ -16,6 +16,7 @@ export function getOptions() {
                     changeEvent: () => {
                         updateQuickRingMenuLayoutLock()
                     },
+                    saveToLocalStorage: true,
                 },
                 spacialAudio: {
                     type: 'CHECKBOX',
