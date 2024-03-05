46d78c6a497575cf55df901b097f58908937cc8d
krypek <krypek@tuta.io>
2024-03-05 17:36:13 +0100

    Update bundle.sh to download new dependencies

--- bundle.sh
+++ bundle.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
-blitzkrieg_version="$(jq '.dependencies["cc-blitzkrieg"]' ccmod.json | tail -c +4 | head -c -2)"
-blitzkrieg_url="https://github.com/krypciak/cc-blitzkrieg/releases/download/v${blitzkrieg_version}/cc-blitzkrieg-${blitzkrieg_version}.ccmod"
-
-dior_version="$(jq '.dependencies["cc-diorbital-menu"]' ccmod.json | tail -c +4 | head -c -2)"
-dior_url="https://github.com/krypciak/cc-diorbital-menu/releases/download/v${dior_version}/cc-diorbital-menu-${dior_version}.ccmod"
-
-inputapi_url='https://github.com/CCDirectLink/input-api/releases/download/v1.0.1/input-api.ccmod'
-ccloader_url="$(curl -s 'https://api.github.com/repos/CCDirectLink/CCLoader/releases/latest' | jq '.tarball_url' | head -c -2 | tail -c +2)"
-
+set -e
+MOD_URLS=(
+    "cc-blitzkrieg" "https://github.com/krypciak/cc-blitzkrieg/releases/download/v@VERSION/cc-blitzkrieg-@VERSION.ccmod"
+    "input-api" "https://github.com/krypciak/input-api/releases/download/v1.0.2/input-api.ccmod"
+    "ccmodmanager" "https://github.com/CCDirectLink/CCModManager/releases/download/v@VERSION/ccmodmanager-@VERSION.ccmod"
+    "nax-ccuilib" "https://github.com/krypciak/nax-ccuilib/releases/download/v1.2.4/nax-ccuilib.ccmod"
+    "nax-module-cache" "https://github.com/krypciak/nax-module-cache/releases/download/v1.0.2/nax-module-cache.ccmod"
+)
 mkdir -p pack/assets/mods
-[ ! -f pack/assets/mods/cc-blitzkrieg* ] && wget "${blitzkrieg_url}" -O "pack/assets/mods/cc-blitzkrieg-${blitzkrieg_version}.ccmod"
-[ ! -f pack/assets/mods/cc-diorbital-menu* ] && wget "${dior_url}" -O "pack/assets/mods/cc-diorbital-menu-${dior_version}.ccmod"
-[ ! -f pack/assets/mods/input-api* ] && wget "${inputapi_url}" -O 'pack/assets/mods/input-api.ccmod'
+for ((i = 0 ; i < ${#MOD_URLS[@]} ; i+=2)); do
+    name=${MOD_URLS[$i]}
+    url=${MOD_URLS[$i+1]}
+    version="$(jq ".dependencies[\"${name}\"]" ccmod.json | tail -c +4 | head -c -2)"
+    url=$(echo $url | sed "s/@VERSION/${version}/g")
+    echo -e "\n----------------Downloading $name v$version"
+    [ ! -f "pack/assets/mods/${name}"* ] && wget -nv "$url" -O "pack/assets/mods/$name.ccmod"
+done
+
 cp crossedeyes*.ccmod pack/assets/mods/
 
-[ ! -f pack/ccloader.tar.gz ] && wget "${ccloader_url}" -O pack/ccloader.tar.gz
+echo -e "\n----------------Downloading CCLoader"
+ccloader_url="$(curl -s 'https://api.github.com/repos/CCDirectLink/CCLoader/releases/latest' | jq '.tarball_url' | head -c -2 | tail -c +2)"
+[ ! -f pack/ccloader.tar.gz ] && wget -nv "${ccloader_url}" -O pack/ccloader.tar.gz
 tar -xf pack/ccloader.tar.gz --directory=pack
 cp -r pack/*CCLoader*/* 'pack/'
 rm -rf pack/*CCLoader*
@@ -24,6 +31,7 @@ rm -rf pack/ccloader.tar.gz
 patch -p1 pack/package.json < .github/workflows/package.patch
 
 cp ./TUTORIAL.md pack/CROSSEDEYES_MANUAL.md
+rm pack/mods.json
 
 cd pack
 rm -rf ../bundle.zip
