bc9a03b779379b93d519bfdd07a2a21557368351
krypek <krypek@tuta.io>
2024-03-12 13:24:37 +0100

    Update workflows and pack.sh

--- pack.sh
+++ pack.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
+set -e
 BASE_NAME="$(jq '.id' ccmod.json | sed 's/^"//;s/"$//')"
 NAME="${BASE_NAME}-$(jq '.version' ccmod.json | sed 's/^"//;s/"$//').ccmod"
 rm -rf "$BASE_NAME"*
 pnpm install
 pnpm run build
 mkdir -p pack
-cp -r assets lang nvdaplugin icon LICENSE plugin.js ./pack
+cp -rf icon LICENSE plugin.js ./pack
+[ -d ./assets ] && cp -r assets ./pack
+[ -d ./lang ] && cp -r lang ./pack
+[ -d ./json ] && cp -r json ./pack
+
 cd ./pack
 for file in $(find . -iname '*.json') $(find . -iname '*.json.patch') $(find . -iname '*.json.patch.cond'); do
     jq '.' ../$file -c > $file
 done
 cp ../ccmod.json .
-rm -rf icon/*.kra icon/*~
+rm -rf icon/icon240.png
+LIST="$(find . -name '*.kra') $(find . -name '*~')"
+rm -rf $LIST
 zip -r "../$NAME" .
 cd ..
 rm -rf pack
